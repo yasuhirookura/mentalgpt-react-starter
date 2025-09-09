0cda15371f0f3a99cecb77f92f82935fbda1798c
github yasuhirookura <baseball.yasu.ohkura@gmail.com>
2025-09-09 23:11:08 +0900

    feat: update firebase.js ログイン 一度だけ効く

--- src/firebase.js
+++ src/firebase.js
@@ -4,7 +4,6 @@ import { getAuth, setPersistence, browserLocalPersistence } from "firebase/auth"
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  // ← ここはあなたのコンソールの値をそのまま
   apiKey: "AIzaSyBG3jGtcLYsYt2X6Zem-W0-r5BdQR14XTI",
   authDomain: "mentalgpt-19189.firebaseapp.com",
   projectId: "mentalgpt-19189",
@@ -20,26 +19,4 @@ export const auth = getAuth(app);
 // ★ 追加：ログイン状態をブラウザに保持
 setPersistence(auth, browserLocalPersistence).catch(console.error);
 
-export const db = getFirestore(app);
-
-/*
-// src/firebase.js
-import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
-
-const firebaseConfig = {
-  // ← ここはあなたのコンソールの値をそのまま
-  apiKey: "AIzaSyBG3jGtcLYsYt2X6Zem-W0-r5BdQR14XTI",
-  authDomain: "mentalgpt-19189.firebaseapp.com",
-  projectId: "mentalgpt-19189",
-  storageBucket: "mentalgpt-19189.firebasestorage.app",
-  messagingSenderId: "159888180556",
-  appId: "1:159888180556:web:6bbc310de7dcb716847be9",
-  measurementId: "G-E70WT63FH5"
-};
-
-const app = initializeApp(firebaseConfig);
-
-export const auth = getAuth(app);
 export const db = getFirestore(app);
