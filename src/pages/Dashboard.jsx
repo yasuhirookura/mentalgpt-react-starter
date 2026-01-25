3eff6cb6504543f3c90f60d3b403d321a9245d59
github yasuhirookura <baseball.yasu.ohkura@gmail.com>
2026-01-25 23:34:37 +0900

    feat: update PetGPT src/pages/Dashboard.jsx

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -221,8 +221,8 @@ border: 1px solid rgba(15,23,42,0.06);
 }
 
 .pgpt__avatar{
-width: 115%;
-height: 115%;
+width: 110%;
+height: 110%;
 object-fit: contain;
 display:block;
 }
@@ -345,8 +345,8 @@ width: 126px;
 height: 126px;
 }
 .pgpt__avatar{
-width: 115%;
-height: 115%;
+width: 110%;
+height: 110%;
 }
 .pgpt__list{
 max-height: 58vh;
